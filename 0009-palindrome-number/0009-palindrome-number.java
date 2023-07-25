21db05ef0627eafaf9154ede5224e01602e5124f
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-07-25 20:14:49 +0700

    Time: 12 ms (16.83%), Space: 42.7 MB (82.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String convertedInt = String.valueOf(x);
-        for(int i=0; i<convertedInt.length()/2; i++){
-            if(convertedInt.charAt(i) != convertedInt.charAt(convertedInt.length()-1-i)){
-                return false;
-            }
-        }
-        return true;
+        StringBuilder sb = new StringBuilder(convertedInt);
+        return convertedInt.equals(sb.reverse().toString());
+        // for(int i=0; i<convertedInt.length()/2; i++){
+        //     if(convertedInt.charAt(i) != convertedInt.charAt(convertedInt.length()-1-i)){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
