aa7c3095a3491be337bded791ada0e9d1c18b538
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-07-26 21:39:52 +0700

    Time: 3 ms (15.73%), Space: 40.5 MB (82.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(stack.empty()){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.peek() == '(' && s.charAt(i) == ')'){
+                    stack.pop();
+                }else if(stack.peek() == '{' && s.charAt(i) == '}'){
+                    stack.pop();
+                }else if(stack.peek() == '[' && s.charAt(i) == ']'){
+                    stack.pop();
+                }else{
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        if(stack.empty()){
+            return true;
+        } return false;
+    }
+}
