c1ec25d1e2afb8948cd101bd146b708f66138e11
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-07-28 09:42:08 +0700

    Time: 0 ms (100.00%), Space: 41.8 MB (55.42%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int res = 0;
+        for(int i=0; i<nums.length; i++){
+            res += nums[i];
+            nums[i] = res;
+        }
+        return nums;
+    }
+}
