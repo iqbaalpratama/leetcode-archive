839119800d90fbaf3f33073d374f86eb87e768d4
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-07-28 08:49:15 +0700

    Time: 0 ms (100.00%), Space: 39.6 MB (27.78%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1 || n==2){
+            return n;
+        }
+        int first = 1;
+        int second = 2;
+        int res = 0;
+        for(int i=3; i<=n; i++){
+            res = first+second;
+            first = second;
+            second = res;
+        }
+        return res;
+    }
+    
+}
