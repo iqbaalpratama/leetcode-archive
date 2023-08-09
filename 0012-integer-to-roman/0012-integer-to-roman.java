a100e19d961bc57ab1255177e3d882ffe2d19838
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-09 17:40:44 +0700

    Time: 14 ms (23.26%), Space: 43.8 MB (35.86%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String intToRoman(int num) {
+        String[] oneDigit = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] twoDigit = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] threeDigit = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] fourDigit = {"", "M", "MM", "MMM"};
+        return fourDigit[(num / 1000)%10] + threeDigit[(num/100)%10] + twoDigit[(num/10)%10] + oneDigit[num%10];
+    }
+}
