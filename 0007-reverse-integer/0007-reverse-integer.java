58b98fa1ca6fa9656d6520634ef8cee156625508
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-09 15:54:20 +0700

    Time: 1 ms (99.69%), Space: 39.8 MB (49.89%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,7 +1,6 @@
 class Solution {
     public int reverse(int x) {
         int result = 0;
-        int currentDigit;
         int signMultiplier = 1;
         if(x < 0){
             signMultiplier = -1;
@@ -11,8 +10,7 @@ class Solution {
             if(result * signMultiplier > Integer.MAX_VALUE / 10 || result * signMultiplier < Integer.MIN_VALUE / 10){
                 return 0;
             }
-            currentDigit = x % 10;
-            result = result * 10 + currentDigit;
+            result = result * 10 + x % 10;
             x /= 10;
         }
         return result * signMultiplier;
