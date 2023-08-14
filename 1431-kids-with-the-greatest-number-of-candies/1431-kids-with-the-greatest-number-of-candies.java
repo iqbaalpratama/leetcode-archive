36a5bc0e6137f08d6f1d6f1881c7e795119aac3b
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-14 12:28:01 +0700

    Time: 1 ms (79.05%), Space: 41.7 MB (46.25%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -8,11 +8,7 @@ class Solution {
             }
         }
         for(int i=0; i<candies.length; i++){
-            if(candies[i] + extraCandies >= max){
-                result.add(true);
-            }else{
-                result.add(false);
-            }
+            result.add(candies[i] + extraCandies >= max);
         }
         return result;
     }
