cacb289592dee660be7a5afe08449279f2a0471c
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-14 17:40:33 +0700

    Time: 9 ms (11.16%), Space: 43.4 MB (15.26%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -11,13 +11,9 @@ class Solution {
     }
     
     private int gcd(int num1, int num2){
-        int gcd = 1;
-        for(int i = 1; i <= num1 && i <= num2; i++)  
-        {     
-            if(num1%i==0 && num2%i==0){
-                gcd = i;     
-            }  
+       if (num2 == 0) {
+        return num1;
         }
-        return gcd;
+        return gcd(num2, num1 % num2);
     }
 }
