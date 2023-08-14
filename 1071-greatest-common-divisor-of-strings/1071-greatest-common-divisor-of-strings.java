783c342ae98801e9a2564ad82e9965c7d88e5740
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-14 18:20:24 +0700

    Time: 1 ms (82.78%), Space: 41.3 MB (45.05%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -3,17 +3,10 @@ class Solution {
         if(!(str1+str2).equals(str2+str1)){
             return "";
         }
-        String result = "";
-        for(int i=1; i <= gcd(str1.length(), str2.length()); i++){
-            result += str1.charAt(i-1);
-        }
-        return result;
+        return str1.substring(0, gcd(str1.length(), str2.length()));
     }
     
     private int gcd(int num1, int num2){
-       if (num2 == 0) {
-        return num1;
-        }
-        return gcd(num2, num1 % num2);
+       return num2 == 0 ? num1 : gcd(num2, num1 % num2);
     }
 }
