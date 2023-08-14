ed2d43c53a2343d38a3130101688b838dc00efdd
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-14 10:51:09 +0700

    Time: 7 ms (28.78%), Space: 41.8 MB (21.69%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,14 +3,10 @@ class Solution {
         int i = 0;
         String result = "";
         while(i < word1.length() || i < word2.length()){
-            if(i>=word1.length()){
-                result += "";
-            }else{
+            if(i<word1.length()){
                 result += word1.charAt(i);
             }
-            if(i>=word2.length()){
-                result += "";
-            }else{
+            if(i<word2.length()){
                 result += word2.charAt(i);
             }
             i++;
