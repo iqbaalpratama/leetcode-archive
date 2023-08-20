06d952f0844305d36b49753f97e91b3d18a142d0
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-08-21 06:01:48 +0700

    Time: 13 ms (91.50%), Space: 39.9 MB (6.56%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -8,17 +8,11 @@ public class Solution extends VersionControl {
         while(start <= end){
             int mid = start+(end-start)/2;
             if(!isBadVersion(mid)){
-                if(isBadVersion(mid+1)){
-                    return mid+1;
-                }
                 start = mid + 1;
             }else{
-                if(!isBadVersion(mid-1)){
-                    return mid;
-                }
                 end = mid - 1;
             }
         }
-        return 0;
+        return start;
     }
 }
