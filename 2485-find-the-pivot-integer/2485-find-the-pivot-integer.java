80898291884a03208b97980766d10b2afa47f364
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-09-11 18:02:48 +0700

    Time: 1 ms (84.42%), Space: 40.3 MB (10.46%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = i+1;
+        }
+        int left = 0;
+        int right = n-1;
+        int sumLeft = arr[left];
+        int sumRight = arr[right];
+        while(left < right){            
+            if(sumLeft < sumRight){
+                left++;
+                sumLeft += arr[left];
+            }else if(sumLeft > sumRight){
+                right--;
+                sumRight += arr[right];
+            }else{
+                left++;
+                right--;
+                sumLeft += arr[left];
+                sumRight += arr[right];
+            }        
+        }
+        if(sumRight == sumLeft && left == right){
+            System.out.println(sumLeft);
+            return left+1;
+        }else{
+            return -1;
+        }
+    }
+}
