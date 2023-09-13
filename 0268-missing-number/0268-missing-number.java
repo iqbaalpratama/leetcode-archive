1818f11ec0f2bd2b4c3de24115495ffb407c8873
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2023-09-13 11:29:45 +0700

    Time: 0 ms (100.00%), Space: 43.8 MB (90.13%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sumAll = nums.length * (nums.length+1) / 2;
+        int sum = 0;
+        for(int i : nums){
+            sum += i;
+        }
+        return sumAll - sum;
+    }
+}
