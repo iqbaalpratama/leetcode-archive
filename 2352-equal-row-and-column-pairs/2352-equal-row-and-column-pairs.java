e043abb6a8816b11510d5ca8ff04286a0ab900a4
Iqbaal Pratama <57170373+iqbaalpratama@users.noreply.github.com>
2024-01-02 16:09:41 +0700

    Time: 98 ms (19.06%), Space: 49.3 MB (62.21%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        Map<String,Integer> mappedPair = new HashMap<>();
+        int n = grid.length;
+        StringBuilder[] colWord = new StringBuilder[n];
+        for(int i=0; i<n; i++){
+            colWord[i] = new StringBuilder();
+        }
+        for(int i=0; i<n; i++){
+            StringBuilder row = new StringBuilder();
+            for(int j=0; j<n; j++){
+                colWord[j].append(grid[i][j]);
+                if(i!=n-1){
+                    colWord[j].append(":");
+                }
+                row.append(grid[i][j]);
+                if(j!=n-1){
+                    row.append(":");
+                }
+            }
+            if(mappedPair.containsKey(row.toString())){
+               mappedPair.put(row.toString(), mappedPair.get(row.toString())+1); 
+            }else{
+                mappedPair.put(row.toString(), 1);
+            }
+        }
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(mappedPair.containsKey(colWord[i].toString())){
+                count += mappedPair.get(colWord[i].toString());
+            }
+        }
+        return count;
+    }
+}
